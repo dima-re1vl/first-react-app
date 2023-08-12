9094af13535a3b6f4a297c805f55107fd1b32485
re1vl <re1vlmail@gmail.com>
2023-08-12 18:14:59 +0300

    edit components

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,20 +1,62 @@
-import React from 'react';
-import s from './Nav.module.css'
-import {NavLink} from 'react-router-dom'
+import React from "react";
+import s from "./Nav.module.css";
+import { NavLink } from "react-router-dom";
 
 const Nav = () => {
-    return (
-        <nav className={s.nav}>
-                <ul>
-                    <li><NavLink to="/profile" className={({ isActive }) => isActive ? s.activeLink : s.item}>Profile</NavLink></li>
-                    <li><NavLink to="/dialogs" className={({ isActive }) => isActive ? s.activeLink : s.item}>Messages</NavLink></li>
-                    <li><NavLink to="/news" className={({ isActive }) => isActive ? s.activeLink : s.item}>News</NavLink></li>
-                    <li><NavLink to="/music" className={({ isActive }) => isActive ? s.activeLink : s.item}>Music</NavLink></li>
-                    <li><NavLink to="/settings" className={({ isActive }) => isActive ? s.activeLink : s.item}>Settings</NavLink></li>
-                </ul>
-                
-        </nav>
-    );
-}
+  return (
+    <nav className={s.nav}>
+      <ul>
+        <li>
+          <NavLink
+            to="/profile"
+            className={({ isActive }) => (isActive ? s.activeLink : s.item)}
+          >
+            Profile
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/dialogs"
+            className={({ isActive }) => (isActive ? s.activeLink : s.item)}
+          >
+            Messages
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/users"
+            className={({ isActive }) => (isActive ? s.activeLink : s.item)}
+          >
+            Users
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/news"
+            className={({ isActive }) => (isActive ? s.activeLink : s.item)}
+          >
+            News
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/music"
+            className={({ isActive }) => (isActive ? s.activeLink : s.item)}
+          >
+            Music
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/settings"
+            className={({ isActive }) => (isActive ? s.activeLink : s.item)}
+          >
+            Settings
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Nav;
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,102 +1,66 @@
-import React from "react";
+import React, { Component } from "react";
 import s from "./Users.module.css";
 import axios from "axios";
 
-let Users = (props) => {
-  if (props.users.length === 0) {
+class Users extends React.Component {
+  componentDidMount() {
     axios
       .get("https://social-network.samuraijs.com/api/1.0/users")
       .then((responce) => {
-        props.setUsers(responce.data.items);
+        this.props.setUsers(responce.data.items);
       });
-
-    //   {
-    //     id: 1,
-    //     fullName: "Dmitriy",
-    //     status: "new account",
-    //     location: { city: "Kiev", country: "Ukraine" },
-    //     followed: false,
-    //     photoURL:
-    //       "https://launchwebsitedesign.com/wp-content/uploads/2017/09/josh-d-avatar.jpg",
-    //   },
-    //   {
-    //     id: 2,
-    //     fullName: "Andrew",
-    //     status: "its me",
-    //     location: { city: "Minsk", country: "Belarus" },
-    //     followed: true,
-    //     photoURL:
-    //       "https://launchwebsitedesign.com/wp-content/uploads/2017/09/josh-d-avatar.jpg",
-    //   },
-    //   {
-    //     id: 3,
-    //     fullName: "Valera",
-    //     status: "hello there",
-    //     location: { city: "Philadelphia", country: "USA" },
-    //     followed: false,
-    //     photoURL:
-    //       "https://launchwebsitedesign.com/wp-content/uploads/2017/09/josh-d-avatar.jpg",
-    //   },
-    //   {
-    //     id: 4,
-    //     fullName: "Misha",
-    //     status: "follow me",
-    //     location: { city: "Kharkov", country: "Ukraine" },
-    //     followed: true,
-    //     photoURL:
-    //       "https://launchwebsitedesign.com/wp-content/uploads/2017/09/josh-d-avatar.jpg",
-    //   },
-    // ]);
   }
 
-  return (
-    <div>
-      {props.users.map((u) => (
-        <div key={u.id}>
-          <span>
-            <div>
-              <img
-                src={
-                  u.uniqueUrlName ||
-                  "https://img.freepik.com/premium-vector/young-smiling-man-avatar-man-with-brown-beard-mustache-hair-wearing-yellow-sweater-sweatshirt-3d-vector-people-character-illustration-cartoon-minimal-style_365941-860.jpg"
-                }
-                className={s.userPhoto}
-              />
-            </div>
-            <div>
-              {u.followed ? (
-                <button
-                  onClick={() => {
-                    props.unFollow(u.id);
-                  }}
-                >
-                  UnFollow
-                </button>
-              ) : (
-                <button
-                  onClick={() => {
-                    props.follow(u.id);
-                  }}
-                >
-                  Follow
-                </button>
-              )}
-            </div>
-          </span>
-          <span>
+  render() {
+    return (
+      <div>
+        {this.props.users.map((u) => (
+          <div key={u.id}>
             <span>
-              <div>{u.name}</div>
-              <div>{u.status}</div>
+              <div>
+                <img
+                  src={
+                    u.uniqueUrlName ||
+                    "https://img.freepik.com/premium-vector/young-smiling-man-avatar-man-with-brown-beard-mustache-hair-wearing-yellow-sweater-sweatshirt-3d-vector-people-character-illustration-cartoon-minimal-style_365941-860.jpg"
+                  }
+                  className={s.userPhoto}
+                />
+              </div>
+              <div>
+                {u.followed ? (
+                  <button
+                    onClick={() => {
+                      this.props.unFollow(u.id);
+                    }}
+                  >
+                    UnFollow
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => {
+                      this.props.follow(u.id);
+                    }}
+                  >
+                    Follow
+                  </button>
+                )}
+              </div>
             </span>
             <span>
-              <div>{"u.location.country"}</div>
-              <div>{"u.location.city"}</div>
+              <span>
+                <div>{u.name}</div>
+                <div>{u.status}</div>
+              </span>
+              <span>
+                <div>{"u.location.country"}</div>
+                <div>{"u.location.city"}</div>
+              </span>
             </span>
-          </span>
-        </div>
-      ))}
-    </div>
-  );
-};
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
 
 export default Users;
